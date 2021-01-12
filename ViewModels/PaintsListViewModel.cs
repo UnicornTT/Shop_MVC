369439d8b8f40543185d268407ff3fdf087043b2
UnicornTT <45918385+UnicornTT@users.noreply.github.com>
2021-01-12 19:43:53 +0300

    Add files via upload

---
+++ ViewModels/PaintsListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.ViewModels
+{
+    //Создается объект, включающий несколько картин для передачи на view
+    public class PaintsListViewModel
+    {
+        public IEnumerable<Paint> AllPaints { get; set; }
+
+        public string currCategory { get; set; }
+    }
+}
