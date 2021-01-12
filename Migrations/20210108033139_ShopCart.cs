e4d635c21995dd4da24a1d82e0d0a4d7efa7c321
UnicornTT <45918385+UnicornTT@users.noreply.github.com>
2021-01-12 19:40:02 +0300

    Add files via upload

---
+++ Migrations/20210108033139_ShopCart.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Migrations
+{
+    public partial class ShopCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShopCartItem",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    paintid = table.Column<int>(type: "int", nullable: true),
+                    price = table.Column<int>(type: "int", nullable: false),
+                    ShopCartId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopCartItem", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ShopCartItem_Paint_paintid",
+                        column: x => x.paintid,
+                        principalTable: "Paint",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopCartItem_paintid",
+                table: "ShopCartItem",
+                column: "paintid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShopCartItem");
+        }
+    }
+}
